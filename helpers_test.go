0d0726ffa6dbed2e4609ef0716cdc83f748c10bd
Jonathan C <jchiang@8thlight.com>
2018-12-01 16:06:43 -0600

    added tests for helper functions

---
+++ helpers_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestCycleActivePlayerIndex(t *testing.T) {
+	t.Run("it increments the index", func(t *testing.T) {
+		playerCount := 2
+		currentIndex := 0
+
+		got := cycleActivePlayerIndex(currentIndex, playerCount)
+		want := 1
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("it resets the index to 0 when reaching the max player count", func(t *testing.T) {
+		playerCount := 2
+		currentIndex := 1
+
+		got := cycleActivePlayerIndex(currentIndex, playerCount)
+		want := 0
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestGetGameStatus(t *testing.T) {
+	t.Run("should return the status of IN_PROGRESS when board is not full", func(t *testing.T) {
+
+	})
+
+	t.Run("should return the status of DRAW when the board is full and no winner", func(t *testing.T) {
+
+	})
+
+	t.Run("should return the status of WINNER when there is a matching pattern", func(t *testing.T) {
+
+	})
+}
