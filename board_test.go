5aaa447afa0362f5e342e88d4c78e99bf75251c4
Jonathan C <jchiang@8thlight.com>
2018-12-01 07:06:57 -0600

    added test for adding position to a full board

--- board_test.go
+++ board_test.go
@@ -20,10 +20,31 @@ func TestAddMove(t *testing.T) {
 	})
 
 	t.Run("add move to a full board", func(t *testing.T) {
+		move1 := Move{"X", 1, "playerId-1"}
+		move2 := Move{"O", 2, "playerId-2"}
+		move3 := Move{"X", 3, "playerId-1"}
+		move4 := Move{"O", 4, "playerId-2"}
+		move5 := Move{"X", 5, "playerId-1"}
+		move6 := Move{"O", 6, "playerId-2"}
+		move7 := Move{"X", 7, "playerId-1"}
+		move8 := Move{"O", 8, "playerId-2"}
+		move9 := Move{"X", 9, "playerId-1"}
+		board := Board{3, []Move{move1, move2, move3, move4, move5, move6, move7, move8, move9}}
+		board.AddMove(Move{"O", 10, "playerId-2"})
 
+		got := board.Moves
+		want := []Move{move1, move2, move3, move4, move5, move6, move7, move8, move9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
 	})
 
 	t.Run("add move to a position that is already taken", func(t *testing.T) {
+
+	})
+
+	t.Run("adding a move to an invalid position", func(t *testing.T) {
 
 	})
 }
