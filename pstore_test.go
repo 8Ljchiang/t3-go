0637afdf136214167c53d51a8f3a51994824e624
Jonathan C <jchiang@8thlight.com>
2018-12-01 09:31:08 -0600

    added test for pstore

---
+++ pstore_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	t.Run("add player to empty pstore", func(t *testing.T) {
+
+	})
+
+	t.Run("add player to store where player already exists", func(t *testing.T) {
+
+	})
+}
+
+func TestGet(t *testing.T) {
+	t.Run("get player from pstore that exists", func(t *testing.T) {
+
+	})
+
+	t.Run("get player from pstore that doesn't exist", func(t *testing.T) {
+
+	})
+}
+
+func TestUpate(t *testing.T) {
+	t.Run("update player that exists in pstore", func(t *testing.T) {
+
+	})
+
+	t.Run("update player that doesn't exist in pstore", func(t *testing.T) {
+
+	})
+}
+
+func TestRemove(t *testing.T) {
+	t.Run("remove player that exists in pstore", func(t *testing.T) {
+
+	})
+
+	t.Run("remove player that doesn't exist in pstore", func(t *testing.T) {
+
+	})
+}
