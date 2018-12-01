d0a916472068b5632e8b760340ca838c631d39a1
Jonathan C <jchiang@8thlight.com>
2018-12-01 06:52:09 -0600

    added initial tests

--- board_test.go
+++ board_test.go
@@ -1,1 +1,17 @@
 package board
+
+import "testing"
+
+func TestAddMove(t *testing.T) {
+	t.Run("add move to empty board", func(t *testing.T) {
+
+	})
+
+	t.Run("add move to a full board", func(t *testing.T) {
+
+	})
+
+	t.Run("add move to a position that is already taken", func(t *testing.T) {
+
+	})
+}
