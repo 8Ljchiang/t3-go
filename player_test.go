37269206083f9a44265d31906f86a665bc0341c6
Jonathan C <jchiang@8thlight.com>
2018-12-01 07:44:33 -0600

    added initial tests

---
+++ player_test.go
@@ -0,0 +1,13 @@
+package player
+
+import "testing"
+
+func TestGetMove(t *testing.T) {
+	t.Run("get move from player", func(t *testing.T) {
+
+	})
+
+	t.Run("get move from bot", func(t *testing.T) {
+
+	})
+}
