ec5fa03b6f57d7b9d4398e32a9c763e1f0150f10
Jonathan C <jchiang@8thlight.com>
2018-12-01 08:01:02 -0600

    added code for player test and player struct

--- board.go
+++ board.go
@@ -1,4 +1,4 @@
-package board
+package main
 
 const (
 	ErrBoardFull            = BoardErr("board is full")
--- player_test.go
+++ player_test.go
@@ -1,13 +1,20 @@
-package player
+package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetMove(t *testing.T) {
 	t.Run("get move from player", func(t *testing.T) {
+		board := Board{3, []Move{}}
+		player := Player{"playerId-1", "John", "X"}
+		move := player.GetMove(board)
 
-	})
+		got := move
+		want := Move{"X", 1, "playerId-1"}
 
-	t.Run("get move from bot", func(t *testing.T) {
-
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 }
