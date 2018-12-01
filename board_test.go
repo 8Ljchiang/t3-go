f3bb5a101f03d2f5b95f7736fad2cbbd51d8d317
Jonathan C <jchiang@8thlight.com>
2018-12-01 06:57:40 -0600

    created add move to empty board test

--- board_test.go
+++ board_test.go
@@ -1,10 +1,22 @@
 package board
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAddMove(t *testing.T) {
 	t.Run("add move to empty board", func(t *testing.T) {
+		board := Board{3, []Move{}}
+		move := Move{"X", 1, "playerId-1"}
+		board.AddMove(move)
 
+		got := board.Moves
+		want := []Move{move}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
 	})
 
 	t.Run("add move to a full board", func(t *testing.T) {
