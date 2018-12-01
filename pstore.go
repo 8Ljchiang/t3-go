e91870bfb51ddd1d3a573a4fff3c7df390696ccf
Jonathan C <jchiang@8thlight.com>
2018-12-01 09:49:45 -0600

    added tests for player store get

--- pstore.go
+++ pstore.go
@@ -1,1 +1,25 @@
 package main
+
+const (
+	ErrPlayerNotFound = PlayerStoreErr("player not found")
+)
+
+type PlayerStoreErr string
+
+func (e PlayerStoreErr) Error() string {
+	return string(e)
+}
+
+type PlayerStore struct {
+	Collection map[string]Player
+}
+
+func (p PlayerStore) Get(id string) (Player, error) {
+	player, ok := p.Collection[id]
+
+	if !ok {
+		return player, ErrPlayerNotFound
+	}
+
+	return player, nil
+}
--- pstore_test.go
+++ pstore_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	t.Run("add player to empty pstore", func(t *testing.T) {
@@ -14,11 +17,30 @@ func TestAdd(t *testing.T) {
 
 func TestGet(t *testing.T) {
 	t.Run("get player from pstore that exists", func(t *testing.T) {
+		playerId := "1"
+		player := Player{Id: playerId, Name: "John", Marker: "X"}
+		collection := map[string]Player{
+			"1": player,
+		}
+		playerStore := PlayerStore{collection}
 
+		got, err := playerStore.Get(playerId)
+		want := player
+
+		assertError(t, err, nil)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 
 	t.Run("get player from pstore that doesn't exist", func(t *testing.T) {
+		playerStore := PlayerStore{map[string]Player{}}
 
+		_, err := playerStore.Get("1")
+		want := ErrPlayerNotFound
+
+		assertError(t, err, want)
 	})
 }
 
