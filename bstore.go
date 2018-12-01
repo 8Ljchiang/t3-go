5c2d952cadbeb520447f381b57513a4cdbfc8450
Jonathan C <jchiang@8thlight.com>
2018-12-01 13:37:59 -0600

    added init BoardStore code

--- bstore.go
+++ bstore.go
@@ -1,1 +1,16 @@
 package main
+
+const (
+	ErrBoardNotFound      = PlayerStoreErr("board not found")
+	ErrBoardAlreadyExists = PlayerStoreErr("board already exists")
+)
+
+type BoardStoreErr string
+
+func (e BoardStoreErr) Error() string {
+	return string(e)
+}
+
+type BoardStore struct {
+	Collection map[string]Board
+}
